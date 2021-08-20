e7f7161eb25fcb955eec950c9fbb9a90a720c78c
qs-154 <qs154152@163.com>
2021-08-20 22:52:29 +0800

    第18次

--- src/test/java/com/usian/test/Test2.java
+++ src/test/java/com/usian/test/Test2.java
@@ -1,4 +1,17 @@
 package com.usian.test;
 
-public class Test2 {    
+public class Test2 {
+    public static class A
+    {
+        public String getString()
+        {
+            return "this is A";
+        }
+    }
+    public static void main(String[] args)
+    {
+        A obj = new A();
+        System.out.println(obj);
+        System.out.println(obj.getString());
+    }
 }
